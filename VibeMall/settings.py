ac5d4401dcde92e636c71cf1f0215a55169f36e5
rajpaladiya2026 <rajpaladiya2023@gmail.com>
2026-04-02 02:17:41 +0200

    update setting.py

--- VibeMall/settings.py
+++ VibeMall/settings.py
@@ -199,7 +199,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = Path(os.getenv('STATIC_ROOT', str(BASE_DIR / 'staticfiles')))
 
 MEDIA_URL = '/media/'
